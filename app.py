5229d1e0822c7bab65bd3c2ca2fa98c998a999ff
Pooja <sachdeva1892001@gmail.com>
2026-02-03 20:09:37 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask , request , jsonify , render_template
+import pandas as pd 
+import pickle
+app = Flask(__name__)
+
+def get_cleaned_data(form_data):
+    gestation = float(form_data["gestation"])
+    parity = int(form_data["parity"])
+    age = float(form_data["age"])
+    weight = float(form_data["weight"])
+    height = float(form_data["height"])
+    smoke = float(form_data["smoke"])
+    cleaned_data = {"gestation":[gestation],
+                    "parity":[parity],
+                    "age":[age],
+                    "height":[height],
+                    "weight":[weight],
+                    "smoke":[smoke]
+                    }
+
+    return cleaned_data
+
+@app.route("/home", methods = ['GET'])
+def home():
+    return render_template("index.html")
+@app.route("/predict" , methods = ['POST'])
+def get_prediction():
+    baby_data_form = request.form
+
+    baby_data_cleaned = get_cleaned_data(baby_data_form)
+
+    bby_df = pd.DataFrame(baby_data_cleaned)
+    with open("myvenv\model\model.pkl", "rb") as obj:
+        model = pickle.load(obj)
+    prediction = model.predict(bby_df)
+    prediction = round(float(prediction[0]) , 2)
+    response = {"Prediction" : prediction}
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
